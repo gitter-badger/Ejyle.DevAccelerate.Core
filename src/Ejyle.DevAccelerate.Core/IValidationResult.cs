4681dbd3ab63fa79142f57de29563c55462d99e5
Tanveer Yousuf <y.tanveer@outlook.com>
2017-05-14 23:08:47 +0530

    Added general-purpose validation interfaces.

---
+++ src/Ejyle.DevAccelerate.Core/IValidationResult.cs
@@ -0,0 +1,18 @@
+﻿// ----------------------------------------------------------------------------------------------------------------------
+// Copyright © Ejyle Technologies (P) Ltd (www.ejyle.com)
+// Licensed under the MIT license. See the LICENSE file in the project's root directory for complete license information.
+// ----------------------------------------------------------------------------------------------------------------------
+
+namespace Ejyle.DevAccelerate.Core
+{
+    /// <summary>
+    /// An interface for a validation result.
+    /// </summary>
+    public interface IValidationResult
+    {
+        /// <summary>
+        /// Determines if the result of the validation is success.
+        /// </summary>
+        bool IsSuccessful { get; }
+    }
+}
